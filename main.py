7ea54c859f0b6e8dd97b79ffaddba9b70dd31962
JSwartzmiller <jswartzm@asu.edu>
2025-06-25 21:08:26 -0400

    New feature update

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from bs4 import BeautifulSoup
+import requests
+import pandas as pd
 
 # Features to scrape:
 # Type of pitch
 #
 # 
 
+# Download the page
+url = "https://www.baseball-reference.com/leagues/majors/2025-standard-pitching.shtml"
+resp = requests.get(url)
+resp.raise_for_status()
+
+soup = BeautifulSoup(resp.text, "html.parser")
+
+comments = soup.find_all(string=lambda text: isinstance(text, BeautifulSoup.Comment))
+
+table = None
+for c in comments:
+    if 'id="players_standard_pitching"' in c:
+        comment_soup = BeautifulSoup(c, "html.parser")
+        table = comment_soup.find("table", {"id": "players_standard_pitching"})
+        break
+
+if table is None:
+    raise ValueError("Couldn't find the standard pitching table on the page.")
+
+df = pd.read_html(str(table))[0]
+
+print(df.head())
+        
